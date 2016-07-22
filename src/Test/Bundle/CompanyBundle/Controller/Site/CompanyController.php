962d97084eb3d4705f6edc5eb9fc61faef913cbf
lubo444 <lubo444@gmail.com>
2016-07-22 10:16:44 +0200

    controllers

---
+++ src/Test/Bundle/CompanyBundle/Controller/Site/CompanyController.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Test\Bundle\CompanyBundle\Controller\Web;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Test\Bundle\CompanyBundle\Entity\Company;
+use Test\Bundle\CompanyBundle\Entity\Week;
+use Test\Bundle\CompanyBundle\Form\FilterType;
+
+use Test\Bundle\CompanyBundle\Form\CompanyType;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class CompanyController extends Controller
+{
+    
+    private function getRoutes(){
+        $router = $this->container->get('router');
+        /** @var $collection \Symfony\Component\Routing\RouteCollection */
+        $collection = $router->getRouteCollection();
+        $allRoutes = $collection->all();
+
+        $routes = array();
+
+        /** @var $params \Symfony\Component\Routing\Route */
+        foreach ($allRoutes as $route => $params)
+        {
+            $defaults = $params->getDefaults();
+
+            if (isset($defaults['_controller']))
+            {
+                $controllerAction = explode(':', $defaults['_controller']);
+                $controller = $controllerAction[0];
+
+                if (!isset($routes[$controller])) {
+                    $routes[$controller] = array();
+                }
+
+                $routes[$controller][]= $route;
+            }
+        }
+
+        return $thisRoutes = isset($routes[get_class($this)]) ?
+                                    $routes[get_class($this)] : null ;
+    }
+    
+    /**
+     * @Route("/{page}", requirements={"page" = "\d+"}, defaults={"page" = 1}, name="test_company_list")
+     * @Template("TestCompanyBundle:Homepage:list.html.twig")
+     */
+    public function companiesListAction(Request $request, $page)
+    {
+        $form = $this->createForm(new FilterType());
+        $form->handleRequest($request);
+        
+        $filters = [];
+        
+        if($this->isGranted('ROLE_ADMIN')){
+            $filters['roleAdmin'] = true;
+        }
+        
+        if ($form->isSubmitted()) {
+            $data = $form->getData();
+
+            $filters['name'] = $data['title'];
+            $filters['day'] = $data['day'];
+            $filters['hour'] = $data['hour'];
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $companies = $em->getRepository('TestCompanyBundle:Company')->getCompanies($filters, $page);
+        
+        return [
+            'companies' => $companies,
+            'daysInWeek' => Week::getDaysInWeek(),
+            'form' => $form->createView(),
+        ];
+    }
+    
+    
+    
+    /**
+     * @Route("/company/create", name="test_company_create")
+     * @Template("TestCompanyBundle:Form:basic.html.twig")
+     */
+    public function registerCompanyAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $loggedUserId = $this->get('test.authorization')->getAuthenticatedUserId();
+
+        $company = new Company();
+        $company->setCreatedBy($loggedUserId);
+        $form = $this->createForm(new CompanyType(), $company);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $title = $form->getData()->getTitle();
+            $company->setTitle($title);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($company);
+            $em->flush();
+
+            return $this->redirectToRoute('test_company_list', array(), 201);
+        }
+
+        return ['form' => $form->createView()];
+    }
+    
+    
+    
+    /**
+     * @Route("/company/{itemId}/edit", requirements={"itemId" = "\d+"}, name="test_company_edit")
+     * @Template("TestCompanyBundle:Form:basic.html.twig")
+     */
+    public function companyEditAction(Request $request, $itemId)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $company = $em->getRepository('TestCompanyBundle:Company')->find($itemId);
+
+        if (!$company) {
+            return $this->get('test.error_manager')->getFlashBagError('Object not found!');
+        }
+
+        $this->get('test.authorization')->checkAccessItem($company);
+
+        $form = $this->createForm(new CompanyType(), $company);
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em->flush();
+
+                return $this->redirectToRoute('test_company_list', array(), 201);
+            }
+        }
+
+        return ['form' => $form->createView()];
+    }
+
+    
+    /**
+     * @Route("/company/{itemId}/delete/{undelete}",
+     *  requirements={"itemId" = "\d+", "undelete" = "\d+"},
+     *  defaults={"undelete" = null},
+     *  name="test_company_delete")
+     * @Template()
+     */
+    public function deleteCompanyAction(Request $request, $itemId, $undelete)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $company = $em->getRepository('TestCompanyBundle:Company')->find($itemId);
+
+        if (!$company) {
+            return $this->get('test.error_manager')->getFlashBagError('Object not found!');
+        }
+
+        $this->get('test.authorization')->checkAccessItem($company);
+
+        if ($undelete) {
+            if ($this->isGranted('ROLE_ADMIN')) {
+                $company->setActive(true);
+            }
+        } else {
+            $company->setActive(false);
+        }
+
+        $em->persist($company);
+        $em->flush();
+
+        return $this->redirectToRoute('test_company_list');
+    }
+
+
+}
---
+++ src/Test/Bundle/CompanyBundle/Controller/Site/OpeningHoursController.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Test\Bundle\CompanyBundle\Controller\Web;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Test\Bundle\CompanyBundle\Entity\Week;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route as BaseRoute;
+use Test\Bundle\CompanyBundle\Entity\OpeningHours;
+use Test\Bundle\CompanyBundle\Form\OpeningHoursType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class OpeningHoursController extends Controller
+{
+
+    /**
+     * @BaseRoute("/office/{officeId}",
+     *  requirements={"officeId" = "\d+"},
+     *  name="test_opnng_hrs")
+     * @Template("TestCompanyBundle:Office:detail.html.twig")
+     */
+    public function openingHoursListAction(Request $request, $officeId)
+    {
+        $cacheManager = $this->get('test.cache_manager');
+
+        $office = $cacheManager->getCachedObject('TestCompanyBundle:Office', $officeId);
+
+        //check parents active status
+        if (!$office || !$office->getActive() || !$office->getIdCompany()->getActive()) {
+            return $this->get('test.error_manager')->getFlashBagError('Object not found!');
+        }
+
+        return [
+            'daysInWeek' => Week::getDaysInWeek(),
+            'office' => $office
+        ];
+    }
+
+    /**
+     * @BaseRoute("/office/{officeId}/opening-hours/create", requirements={"officeId" = "\d+"}, name="test_opening_hours_create")
+     * @Template("TestCompanyBundle:Form:basic.html.twig")
+     */
+    public function registerOpeningHoursAction(Request $request, $officeId)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $loggedUserId = $this->get('test.authorization')->getAuthenticatedUserId();
+        $opnngHours = new OpeningHours();
+        
+        $opnngHours->setCreatedBy($loggedUserId);
+
+        $form = $this->createForm(new OpeningHoursType(), $opnngHours);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $startAt = $data->getStartAt();
+            $startLunchAt = $data->getLunchStartAt();
+            $endLunchAt = $data->getLunchEndAt();
+            $endAt = $data->getEndAt();
+
+            $office = $em->getRepository('TestCompanyBundle:Office')->find($officeId);
+
+            $opnngHours->setIdOffice($office);
+            $opnngHours->setStartAt($startAt);
+            $opnngHours->setLunchStartAt($startLunchAt);
+            $opnngHours->setLunchEndAt($endLunchAt);
+            $opnngHours->setEndAt($endAt);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($opnngHours);
+            $em->flush();
+
+            return $this->redirectToRoute('test_opnng_hrs', array('officeId' => $officeId), 201);
+        }
+
+        return ['form' => $form->createView()];
+    }
+
+    /**
+     * @BaseRoute("/opening-hours/{itemId}/edit", requirements={"itemId" = "\d+"}, name="test_opening_hours_edit")
+     * @Template("TestCompanyBundle:Form:basic.html.twig")
+     */
+    public function editOpeningHoursAction(Request $request, $itemId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $cacheManager = $this->get('test.cache_manager');
+
+        $opnngHours = $em->getRepository('TestCompanyBundle:OpeningHours')->find($itemId);
+        if (!$opnngHours) {
+            return $this->get('test.error_manager')->getFlashBagError('Object not found!');
+        }
+
+        $this->get('test.authorization')->checkAccessItem($opnngHours);
+
+        $form = $this->createForm(new OpeningHoursType(), $opnngHours);
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em->flush();
+
+                $officeId = $form->getData()->getIdOffice()->getIdOffice();
+
+                $cacheManager->updateCachedObject('TestCompanyBundle:Office', $officeId);
+
+                return $this->redirectToRoute('test_opnng_hrs', ['officeId' => $officeId], 201);
+            }
+        }
+
+        return ['form' => $form->createView()];
+    }
+
+    /**
+     * @BaseRoute("/opening-hours/{itemId}/delete/{undelete}",
+     *  requirements={"itemId" = "\d+", "undelete" = "\d+"},
+     *  defaults={"undelete" = null},
+     *  name="test_opening_hours_delete")
+     * @Template()
+     */
+    public function deleteOpeningHoursAction(Request $request, $itemId, $undelete)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $openingHours = $em->getRepository('TestCompanyBundle:OpeningHours')->find($itemId);
+
+        if (!$openingHours) {
+            return $this->get('test.error_manager')->getFlashBagError('Object not found!');
+        }
+
+        $this->get('test.authorization')->checkAccessItem($openingHours);
+
+        if ($undelete) {
+            if ($this->isGranted('ROLE_ADMIN')) {
+                $openingHours->setActive(true);
+            }
+        } else {
+            $openingHours->setActive(false);
+        }
+
+        $officeId = $openingHours->getIdOffice()->getIdOffice();
+
+        $em->persist($openingHours);
+        $em->flush();
+
+        return $this->redirectToRoute('test_opnng_hrs', array('officeId' => $officeId), 301);
+    }
+
+}

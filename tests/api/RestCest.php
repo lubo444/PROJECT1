c440349cb5cccd15e721c3d55459d87d9eb02820
lubo444 <lubo444@gmail.com>
2016-08-16 14:27:20 +0200

    rest api new test

---
+++ tests/api/RestCest.php
@@ -0,0 +1,53 @@
+<?php
+
+class RestCest
+{
+
+    public function _before(ApiTester $I)
+    {
+        
+    }
+
+    public function _after(ApiTester $I)
+    {
+        
+    }
+
+    public function tryToTest(ApiTester $I)
+    {
+        $I->sendGET('/companies/54/offices', []);
+        $I->seeResponseCodeIs(200);
+
+        $token = 'NDU1OWZkYmJkODNmZTQwNzE3YTA0MThkYjk4ZDg5MzNkOGY2Yjc5MjEzYWQwZGE5YzM3ZTg4ZTIxNTI0ZjIyMw';
+
+        $I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
+        $I->amBearerAuthenticated($token);
+
+        $I->sendPOST('/companies', ['title' => 'codeceptionTest']);
+        $I->seeResponseCodeIs(201);
+
+        $I->sendPUT('/companies/54', ['title' => 'Qwerty1']);
+        $I->seeResponseCodeIs(200);
+
+        $I->sendPATCH('/companies/54', ['title' => 'Qwerty2']);
+        $I->seeResponseCodeIs(200);
+
+        $I->sendDELETE('/companies/59', []);
+        $I->seeResponseCodeIs(200);
+
+        $I->sendPUT('/companies/58/undelete', []);
+        $I->seeResponseCodeIs(200);
+
+        $I->deleteHeader('Content-Type');
+
+        /*
+          $I->amHttpAuthenticated('service_user', '123456');
+          $I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
+          $I->sendPOST('users', ['name' => 'davert', 'email' => 'davert@codeception.com']);
+          $I->seeResponseCodeIs(200);
+          $I->seeResponseIsJson();
+          $I->seeResponseContains('{"result":"ok"}');
+          /* */
+    }
+
+}
